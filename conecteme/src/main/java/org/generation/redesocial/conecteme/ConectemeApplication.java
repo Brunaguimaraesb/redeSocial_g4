a39863ee342f0df697142d92fa0e1cc56f446d87
alysoncampos <alyson.siqueiracampos@gmail.com>
2021-11-04 09:17:59 -0300

    @alysoncampos[PROJETO INTEGRADOR ADICIONADO]

---
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/ConectemeApplication.java
@@ -0,0 +1,13 @@
+package org.generation.redesocial.conecteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectemeApplication.class, args);
+	}
+
+}
