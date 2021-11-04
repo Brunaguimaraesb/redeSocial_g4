29c8be12ecf3a72cbaf02e283498ec5b08624df9
alysoncampos <alyson.siqueiracampos@gmail.com>
2021-11-04 17:21:13 -0300

    @alysoncampos[task: CREATE MODEL]

---
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/model/TemaModel.java
@@ -0,0 +1,63 @@
+package org.generation.redesocial.conecteme.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class TemaModel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idTema;
+	
+	@NotBlank
+	private String tipo;
+	
+	@NotBlank
+	@Size(min = 10, max=255)
+	private String descricao;
+	
+	@NotBlank
+	private String foto;
+
+	public long getIdTema() {
+		return idTema;
+	}
+
+	public void setIdTema(long idTema) {
+		this.idTema = idTema;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+
+}
