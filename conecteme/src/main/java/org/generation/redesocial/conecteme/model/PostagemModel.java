1e5705dd896c37bc69a38dbf4471d2b2e13400de
brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2021-11-10 14:31:43 -0300

    @Brunaguimaraes [Task: CREATE PostagemModel]

---
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/model/PostagemModel.java
@@ -0,0 +1,77 @@
+package org.generation.redesocial.conecteme.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class PostagemModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idPostagem;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+	@NotBlank
+	@Size(min = 5, max = 100)
+	private String titulo;
+
+	@NotBlank
+	@Size(min = 5, max = 500)
+	private String descricao;
+
+	@NotBlank
+	private String foto;
+
+	public long getIdPostagem() {
+		return idPostagem;
+	}
+
+	public void setIdPostagem(long idPostagem) {
+		this.idPostagem = idPostagem;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
