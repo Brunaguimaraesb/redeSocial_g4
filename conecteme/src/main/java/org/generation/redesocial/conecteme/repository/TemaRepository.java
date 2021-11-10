2ea9eb010f5720adc4ead7f64555e79a6ebfb21d
RenanUmV <renan.uchoa.m@gmail.com>
2021-11-10 15:40:09 -0300

    @RenanUmV [Task: CREATE PostagemController]

--- conecteme/src/main/java/org/generation/redesocial/conecteme/repository/TemaRepository.java
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/repository/TemaRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.generation.redesocial.conecteme.model.TemaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TemaRepository extends JpaRepository<TemaModel, Long> {
 	public List<TemaModel> findAllByTipoContainingIgnoreCase(String tipo);
 }
