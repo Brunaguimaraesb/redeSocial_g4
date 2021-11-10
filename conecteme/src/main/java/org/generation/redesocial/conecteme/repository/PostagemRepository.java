0a61c0de26ddcb1a14d0dfb2d1f1b0e5ad82e61e
brunaguimaraesb <brunaguimaraes.santos@hotmail.com>
2021-11-10 14:54:57 -0300

    @Brunaguimaraesb [task: UPDATE PostagemRepository]

--- conecteme/src/main/java/org/generation/redesocial/conecteme/repository/PostagemRepository.java
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/repository/PostagemRepository.java
@@ -1,5 +1,10 @@
 package org.generation.redesocial.conecteme.repository;
 
-public interface PostagemRepository {
+import java.util.List;
 
+import org.generation.redesocial.conecteme.model.PostagemModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostagemRepository extends JpaRepository<PostagemModel, Long>{
+	public List<PostagemModel> findAllByTituloContainingIgnoreCase(String titulo);
 }
