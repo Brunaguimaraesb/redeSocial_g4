27077fafdf2d09f8d62ffa0d80917f06543d2562
Ályson Campos <85349942+alysoncampos@users.noreply.github.com>
2021-11-17 16:25:06 -0300

    Revert "@RenanUmV [Task: CREATED USERS CONTROLLER AND FIXED BUGS]"

--- conecteme/src/main/java/org/generation/redesocial/conecteme/controller/TemaController.java
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/controller/TemaController.java
@@ -2,8 +2,8 @@ package org.generation.redesocial.conecteme.controller;
 
 import java.util.List;
 
-import org.generation.redesocial.conecteme.model.UsuarioModel;
-import org.generation.redesocial.conecteme.repository.UsuarioRepository;
+import org.generation.redesocial.conecteme.model.TemaModel;
+import org.generation.redesocial.conecteme.repository.TemaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,41 +18,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/temas")
 @CrossOrigin("*")
-public class UsuarioController {
+public class TemaController {
 
 	@Autowired
-	private UsuarioRepository repository;
-
+	private TemaRepository repository;
+	
 	@GetMapping
-	public ResponseEntity<List<UsuarioModel>> GetAll() {
+	public ResponseEntity<List<TemaModel>> GetAll(){
 		return ResponseEntity.ok(repository.findAll());
-
 	}
-
+	
 	@GetMapping("/{id}")
-	public ResponseEntity<UsuarioModel> GetById(@PathVariable long id) {
+	public ResponseEntity<TemaModel> GetById(@PathVariable long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
-
 	}
 
-	@GetMapping("/nome/{nome}")
-	public ResponseEntity<List<UsuarioModel>> GetByNome(@PathVariable String nome) {
-		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	@GetMapping("/tipo/{tipo}")
+	public ResponseEntity<List<TemaModel>> GetByTipo(@PathVariable String tipo){
+		return ResponseEntity.ok(repository.findAllByTipoContainingIgnoreCase(tipo));
+		
 	}
-
+	
 	@PostMapping
-	public ResponseEntity<UsuarioModel> post(@RequestBody UsuarioModel usuario) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuario));
+	public ResponseEntity<TemaModel> post (@RequestBody TemaModel tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(tema));
 	}
-
+	
 	@PutMapping
-	public ResponseEntity<UsuarioModel> put(@RequestBody UsuarioModel usuario) {
-		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuario));
-
+	public ResponseEntity<TemaModel> put (@RequestBody TemaModel tema) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(tema));
 	}
-
+		
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable long id) {
 		repository.deleteById(id);
