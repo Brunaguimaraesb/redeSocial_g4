fdd516ec00161a35f5764923b28e970025ac1f6f
RenanUmV <renan.uchoa.m@gmail.com>
2021-11-22 10:37:31 -0300

    @RenanUmV [Task: Update Repositories]

--- conecteme/src/main/java/org/generation/redesocial/conecteme/repository/UsuarioRepository.java
+++ conecteme/src/main/java/org/generation/redesocial/conecteme/repository/UsuarioRepository.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, Long> {
-	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
 	
-	public Optional<UsuarioModel> findByEmail(String email); 
+	public Optional<UsuarioModel> findByUsuario(String usuario); 
+	
+	public Optional<UsuarioModel> findByNome(String nome);
+	
+	public List<UsuarioModel> findAllByNomeContainingIgnoreCase(String nome);
 
 }
